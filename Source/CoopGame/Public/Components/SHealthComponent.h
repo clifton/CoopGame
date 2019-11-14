c0d55fbd8365363521c70b7022238878c4d465ba
Clifton King <cliftonk@gmail.com>
2019-11-14 12:20:04 -0500

    create death event in health component

--- Source/CoopGame/Public/Components/SHealthComponent.h
+++ Source/CoopGame/Public/Components/SHealthComponent.h
@@ -23,16 +23,22 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Events")
 	FOnHealthChangedSignature OnHealthChanged;
 
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHealthChangedSignature OnDeath;
+
 	USHealthComponent();
 
 protected:
-	virtual void BeginPlay() override;
-
 	UPROPERTY(BlueprintReadOnly, Category = "HealthComponent")
 	float Health;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HealthComponent")
 	float DefaultHealth;
+
+	UPROPERTY(BlueprintReadOnly, Category = "HealthComponent")
+	bool bIsDead;
+
+	virtual void BeginPlay() override;
 
 	UFUNCTION()
 	void HandleTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
