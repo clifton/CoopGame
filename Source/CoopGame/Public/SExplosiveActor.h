2e90d9b2cd48e199fee1b513b72b406cac0aee16
Clifton King <cliftonk@gmail.com>
2019-11-14 16:55:01 -0500

    added C++ implementation of explosive objects

---
+++ Source/CoopGame/Public/SExplosiveActor.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveActor.generated.h"
+
+
+class USHealthComponent;
+class URadialForceComponent;
+class UParticleSystem;
+
+
+UCLASS()
+class COOPGAME_API ASExplosiveActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	ASExplosiveActor();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USHealthComponent* HealthComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	URadialForceComponent* RadialForceComp;
+
+	bool bExploded;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float ExplosionImpulse;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float ExplosionRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	float ExplosionDamage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	UParticleSystem* ExplosionFX;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Explosion")
+	UMaterialInterface* ExplodedMaterial;
+
+	UFUNCTION()
+	void OnDeath(
+		USHealthComponent* ChangedHealthComp, float Health, float HealthDelta,
+		const class UDamageType* DamageType,
+		class AController* InstigatedBy, AActor* DamageCauser);
+
+	virtual void BeginPlay() override;
+
+};
