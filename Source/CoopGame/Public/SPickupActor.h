b2f080a9a0ed52377140b9378fa1c6f7787beb2a
Clifton King <cliftonk@gmail.com>
2019-11-18 17:11:05 -0500

    add health regen powerup

--- Source/CoopGame/Public/SPickupActor.h
+++ Source/CoopGame/Public/SPickupActor.h
@@ -26,7 +26,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UDecalComponent* DecalComp;
 
-	UPROPERTY(EditDefaultsOnly, Category = "PickupActor")
+	UPROPERTY(EditInstanceOnly, Category = "PickupActor")
 	TSubclassOf<ASPowerupActor> PowerupClass;
 
 	UPROPERTY(EditDefaultsOnly, Category = "PickupActor")
